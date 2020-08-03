10f3fe843f31b57d1a5f85cc868e426f464a72ad
Lewin Pandey <lewin.pandey01@gmail.com>
2020-08-03 20:59:19 +0545

    define typescript in marketing automation component

---
+++ src/components/MarketingAutomation/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import {styled} from "theme";
+import { MarketingAutomationProps } from "./types"
+import {Heading,SubHeading,Image} from "components";
+
+
---
+++ src/components/MarketingAutomation/types.d.ts
@@ -0,0 +1,8 @@
+export interface MarketingAutomationProps{
+    heading:string;
+    subHeading:string;
+    image:{
+        imageSrc:string;
+        altText:string;
+    };
+}
